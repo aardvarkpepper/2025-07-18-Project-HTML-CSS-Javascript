94b36d89444a7a3d39c820f17105979d0de1c618
James Lee <aardvarkpepper@gmail.com>
2025-07-19 19:42:58 -0400

    Used git revert to return to last working state.

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,30 @@
-import { server } from './server/server.js';
+//import { server } from './server/server.js';
 
-console.log(server());
+//console.log(server());
 
-// var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([51.505, -0.09], 13);
 
-// //console.log(map);
+//console.log(map);
 
-// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//   maxZoom: 19,
-//   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-// }).addTo(map);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
 
-// var LeafIcon = L.Icon.extend({
-//   options: {
-//     iconSize: [48, 56],
-//     iconAnchor: [22, 94],
-//     popupAnchor: [-3, -76]
-//   }
-// });
+var LeafIcon = L.Icon.extend({
+  options: {
+    iconSize: [48, 56],
+    iconAnchor: [22, 94],
+    popupAnchor: [-3, -76]
+  }
+});
 
-// var greenIcon = new LeafIcon({ iconUrl: './images/icon-location.png' });
-// //note:  iconUrl is relative to *index.html*.
+var greenIcon = new LeafIcon({ iconUrl: './images/icon-location.png' });
+//note:  iconUrl is relative to *index.html*.
 
-// L.icon = function (options) {
-//   return new L.Icon(options);
-// };
+L.icon = function (options) {
+  return new L.Icon(options);
+};
 
-// L.marker([51.5, -0.09], { icon: greenIcon }).addTo(map).bindPopup("IP address location.");
+L.marker([51.5, -0.09], { icon: greenIcon }).addTo(map).bindPopup("IP address location.");
 
